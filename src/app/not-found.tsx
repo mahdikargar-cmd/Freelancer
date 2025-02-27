c440784527d912615909bc09b468c95c03d3a5ea
Mohammad Halimi <mohammadhalimi.2001@gmail.com>
2025-02-27 23:15:08 +0330

    delete style.css

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import './Notfound.css';
 function NotFound() {
     return (
         <div className='custom flex justify-between'>
-            <div className='text-red-500' title="404">404</div>
-            <div className='text-red-500' title='404'>error</div>
+            <p className="text-red-500">
+                404 not found
+            </p>
         </div>
 
     )
