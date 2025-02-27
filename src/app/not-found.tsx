8837a17a05d1a7c75dda8723c5e92af6dfac4b11
mahdi <www.mahdikargar@gmail.com>
2025-02-27 22:46:23 +0330

    add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Notfound.css';
+function NotFound() {
+    return (
+        <div className='custom flex justify-between'>
+            <div className='fo' title="404">404</div>
+            <div className='fo' title='404'>error</div>
+        </div>
+
+    )
+}
+
+export default NotFound
