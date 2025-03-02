c674ea1f3c1cbcf9e4c034e12f4167caa87f4d43
mahdi <www.mahdikargar@gmail.com>
2025-03-02 13:26:16 +0330

    complete 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,12 @@
-function NotFound() {
-    return (
-        <div className='text-center font-primaryBold'>
-            <p className="text-red-500">
-                صفحه مورد نظر پیدا نشد.
-            </p>
-        </div>
+import React from 'react';
 
-    )
+function NotFound() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-black text-white text-[96px] font-mono tracking-tighter relative">
+      <div className="relative inline-block animate-bounce" title="404">404</div>
+      <div className="relative inline-block animate-bounce" title="error">error</div>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
