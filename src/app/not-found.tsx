0febc0018be2dadc172bf97066ff6d191abe2285
Mohammad Halimi <mohammadhalimi.2001@gmail.com>
2025-03-05 00:27:33 +0330

    404 page is done

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,30 @@
-const NotFound = () => {
+import API from "@/components/api";
+
+interface Link {
+  id: number,
+  first: string,
+  second: string
+}
+
+interface list {
+  list: Link[]
+}
+
+const NotFound = async () => {
+  const res = await fetch(`${API}/api/notfound`, { cache: "no-store" });
+  const posts : list = await res.json();
+  
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-black text-white text-[96px] font-mono tracking-tighter relative">
-      <div className="relative inline-block animate-bounce" title="404">404</div>
-      <div className="relative inline-block animate-bounce" title="error">error</div>
-    </div>
+    <>
+      {
+        posts.list.map((items) => (
+          <div key={items.id} className="flex items-center justify-center w-screen h-screen bg-black text-white text-[96px] font-mono tracking-tighter relative">
+            <div className="relative inline-block animate-bounce" title="404">{items.first}</div>
+            <div className="relative inline-block animate-bounce" title="error">{items.second}</div>
+          </div>
+        ))
+      }
+    </>
   );
 }
 
