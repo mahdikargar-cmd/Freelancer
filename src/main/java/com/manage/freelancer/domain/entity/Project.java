9fa80689bba603b10ee8d926ddaad3a0e5a0318f
mahdi <www.mahdikargar@gmail.com>
2025-03-21 07:03:22 +0330

    update

---
+++ src/main/java/com/manage/freelancer/domain/entity/Project.java
@@ -0,0 +1,14 @@
+package com.manage.freelancer.domain.entity;
+
+import com.manage.freelancer.AAA.infrastructure.entity.UserDTO;
+
+public class Project {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String profileImageUrl;
+    private String address;
+    private String placeOfStudy;
+    private UserDTO user;
+}
