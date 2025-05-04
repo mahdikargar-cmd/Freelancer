1736a3fd6f52c0df37a164ca7894f3d83760adc5
Mahdi <mahdikargar456@gmail.com>
2025-05-04 22:03:00 +0330

    update metaData

--- frontend/src/app/(with-header-footer)/page.tsx
+++ frontend/src/app/(with-header-footer)/page.tsx
@@ -5,6 +5,17 @@ export const metadata = {
     description: "در ددلاین پروژه‌های برنامه‌نویسی، طراحی و تولید محتوا را به بهترین فریلنسرها بسپارید.",
     keywords: ["ددلاین", "freelancer", "فریلنسری", "انجام پروژه", "deadline", "پروژه دانشجویی","project","فریلنسر","فریلنسری"
     ,"طراحی","برنامه نویسی","deadlinee","deadlinee.ir"],
+    alternates: {
+        canonical: "https://deadlinee.ir/",
+    },
+    openGraph: {
+        title: "ددلاین | بازار فریلنسری ایران",
+        description: "بهترین مکان برای برون‌سپاری پروژه‌ها به فریلنسرهای متخصص",
+        url: "https://deadlinee.ir/",
+        siteName: "ددلاین",
+        locale: "fa_IR",
+        type: "website",
+    },
 };
 
 export default function Home() {
---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,21 @@
+// app/head.tsx
+export default function Head() {
+    return (
+        <>
+            <meta name="description" content="سایت ددلاین، ارائه‌دهنده خدمات فریلنسری ایرانی..." />
+            <meta name="keywords" content="فریلنسری, ددلاین, فریلنسر, برنامه نویسی, طراحی, انجام پروژه" />
+            <meta name="robots" content="index, follow" />
+            <meta name="language" content="fa" />
+            <meta property="og:locale" content="fa_IR" />
+            <meta property="og:title" content="ددلاین | سیستم فریلنسری ایرانی" />
+            <meta property="og:description" content="با ددلاین پروژه‌های خود را به متخصص‌ترین فریلنسرهای ایرانی بسپارید" />
+            <meta property="og:url" content="https://deadlinee.ir/" />
+            <meta property="og:site_name" content="ددلاین" />
+            <link rel="icon" href="/public/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+        </>
+    );
+}
